107beed30f97657bdf6dc5ef2dd45271037fed51
Ricardo Semensato <rtestonsemensato@gmail.com>
2021-04-25 14:31:52 -0300

    Subindo o projeto

---
+++ src/pages/_app.tsx
@@ -0,0 +1,24 @@
+import "../styles/global.scss";
+
+import React, { useState } from "react";
+import { Header } from "../components/Header";
+import { Player } from "../components/Player";
+
+import styles from "../styles/app.module.scss";
+import { PlayerContext, PlayerContextProvider } from "../contexts/PlayerContext";
+
+function MyApp({ Component, pageProps }) {
+    return (
+        <PlayerContextProvider>
+            <div className={styles.wrapper}>
+                <main>
+                    <Header />
+                    <Component {...pageProps} />
+                </main>
+                <Player />
+            </div>
+        </PlayerContextProvider>
+    );
+}
+
+export default MyApp;
